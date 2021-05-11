b75f07a4ed5a6cb78f6041657c69da1c6b5b13cb
Guilherme Sprorowski <guilherme.97.gis@gmail.com>
2021-05-11 17:56:39 -0300

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'imc-angular';
+export class AppComponent implements OnInit {
+  form;
+  
+  constructor(private formBuilder: FormBuilder) {}
+
+  resultado = 0;
+  ngOnInit(): void {    
+    this.form = this.formBuilder.group({
+      altura:[],
+      peso: []
+     })
+  }
+
+  calcular(){
+    let peso = this.form.get('peso').value;    
+    let altura = this.form.get('altura').value / 100;
+
+    this.resultado =  peso / (altura * altura)
+  }
 }
